e83a58a0258d4ecc399336cc5d61c72fe77ea17f
mandolyte <cecil.new@gmail.com>
2017-03-18 09:03:11 -0400

    completed

--- recursecsv/recursecsv.go
+++ recursecsv/recursecsv.go
@@ -7,14 +7,21 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
+	"strings"
 )
 
+var w *csv.Writer
+
+var parent = flag.Int("parent", 1, "Parent column; default 1")
+var child = flag.Int("child", 2, "Child column; default 2")
+var start = flag.String("start", "", "Start value of hierarchy")
+var delimiter = flag.String("delimiter", ">", "String for path delimiter")
+var input = flag.String("i", "", "Input CSV filename; default STDIN")
+var output = flag.String("o", "", "Output CSV filename; default STDOUT")
+var help = flag.Bool("help", false, "Show usage message")
+
 func main() {
-	parent := flag.Int("parent", 1, "Parent column; default 1")
-	child := flag.Int("child", 2, "Child column; default 2")
-	input := flag.String("i", "", "Input CSV filename; default STDIN")
-	output := flag.String("o", "", "Output CSV filename; default STDOUT")
-	help := flag.Bool("help", false, "Show usage message")
 	flag.Parse()
 
 	if *help {
@@ -22,7 +29,6 @@ func main() {
 	}
 
 	// open output file
-	var w *csv.Writer
 	if *output == "" {
 		w = csv.NewWriter(os.Stdout)
 	} else {
@@ -62,24 +68,71 @@ func main() {
 			break
 		}
 		if rerr != nil {
-			log.Fatalf("csv.Read:\n%v\n", rerr)
+			log.Fatalf("csv.Read [row %v]:\n%v\n", row, rerr)
 		}
 		if row == 0 {
 			recurseHeaders[1] = cells[pcol]
 			recurseHeaders[2] = cells[ccol]
+			writeRow(recurseHeaders[0], recurseHeaders[1],
+				recurseHeaders[2], recurseHeaders[3], recurseHeaders[4])
 			row++
 			continue
 		}
 		_, ok := parents[cells[pcol]]
 		if ok {
-			parents[cells[pcol]] = append(parents[cells[pcol]],cells[ccol])
+			parents[cells[pcol]] = append(parents[cells[pcol]], cells[ccol])
 		} else {
-			parents[cells[pcol]] = make([]string,0)
-			parents[cells[pcol]] = append(parents[cells[pcol]],cells[ccol])
+			parents[cells[pcol]] = make([]string, 0)
+			parents[cells[pcol]] = append(parents[cells[pcol]], cells[ccol])
 		}
 		row++
 	}
+
+	recurse(0, *start, *delimiter+*start, parents)
+
 	w.Flush()
+}
+
+func recurse(level int, start, path string, parents map[string][]string) {
+	// get value from map for start node
+	v, ok := parents[start]
+	if !ok {
+		return // at a leaf node
+	}
+
+	// sort the children
+	sort.Strings(v)
+
+	level++ // increment depth
+	for _, child := range v {
+		looptest := *delimiter + child + *delimiter
+		cycle := "No"
+		if strings.Contains(path, looptest) {
+			cycle = "Yes"
+		}
+		sLevel := fmt.Sprintf("%v", level)
+		sPath := path + *delimiter + child
+		writeRow(sLevel, start, child, sPath, cycle)
+		if cycle == "No" {
+			recurse(level, child, sPath, parents)
+		}
+	}
+
+}
+
+func writeRow(level, parent, child, path, cycle string) {
+	var cells []string
+	cells = append(cells, level)
+	cells = append(cells, parent)
+	cells = append(cells, child)
+	cells = append(cells, path)
+	cells = append(cells, cycle)
+
+	err := w.Write(cells)
+	if err != nil {
+		log.Fatalf("csv.Write:\n%v\n", err)
+	}
+
 }
 
 func usage(msg string) {
@@ -91,5 +144,6 @@ func usage(msg string) {
 var recurseHeaders []string
 
 func init() {
-	recurseHeaders = append(recurseHeaders, "level", "", "", "Path", "Cycle")
+	recurseHeaders = append(recurseHeaders,
+		"Level", "", "", "Path", "Cycle")
 }
